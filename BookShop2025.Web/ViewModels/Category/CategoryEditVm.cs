3c27ce99fed047e73deecfbee1cf816e0c9be180
CarlosCatedras <carloscatedras43@gmail.com>
2025-06-12 18:35:15 -0300

    ultima version

--- BookShop2025.Web/ViewModels/Category/CategoryEditVm.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BookShop2025.Web.ViewModels.Category
-{
-    public class CategoryEditVm
-    {
-        public int CategoryId { get; set; }
-        public string CategoryName { get; set; } = null!;
-    }
-}
