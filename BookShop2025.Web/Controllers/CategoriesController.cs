cb51962bcdbe7c0be11118a71ba56fc74895a5de
CarlosCatedras <carloscatedras43@gmail.com>
2025-06-12 13:11:26 -0300

    Vista de la lista

---
+++ BookShop2025.Web/Controllers/CategoriesController.cs
@@ -0,0 +1,21 @@
+﻿using BookShop2025.Data.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookShop2025.Web.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public IActionResult Index()
+        {
+            var categories = _categoryService.GetAll();
+            return View(categories);
+        }
+    }
+}
